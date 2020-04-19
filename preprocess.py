8e76f67ff4004ade5da3abf512eb8ec52ec814ba
Riley Valencia <47699505+rpvalencia@users.noreply.github.com>
2020-04-19 14:15:03 -0400

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -10,7 +10,7 @@ ps = PorterStemmer()
 stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
 
 keywords = ['china', 'wuhan', 'covid', 'coronavirus', 'sarscov']
-repository = {'china': 'chinaData.json', 'wuhan': 'wuhanData.json', 'covid': 'covidData.json', 'coronavirus': 'coronavirusData.json', 'sarscov': 'sarsCovData.json'}
+repository = 'processedData.json'
 
 
 tweetIds = []
@@ -55,4 +55,4 @@ data = {}
 for keyword in keywords:
 	print("Processing {}".format(keyword))
 	data[keyword] = {}
-	data[keyword]['content'], data[keyword]['likes'], data[keyword]['retweets'], data[keyword]['vocab'] = preprocessData(repository[keyword])
+	data[keyword]['content'], data[keyword]['likes'], data[keyword]['retweets'], data[keyword]['vocab'] = preprocessData(repository)
